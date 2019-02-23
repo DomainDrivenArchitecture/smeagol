8f0d8e430b945c99269d0a41fc8855eddb6d076d
Vlad Bokov <razum2um@mail.ru>
2019-02-24 03:24:37 +0700

    Add some security by ns whitelisting

--- src/smeagol/testing.clj
+++ src/smeagol/testing.clj
@@ -1,37 +1,8 @@
 (ns smeagol.testing
   (:require [clojure.string :as string]
             [clojure.edn :as edn]
-            [clojure.edn :as edn]
-            [nrepl.core :as nrepl]))
-
-(defn- call-with-require [fn-name in]
-  (let [ns-sym (some-> fn-name namespace symbol)]
-    `(do ~(when ns-sym `(require (quote ~ns-sym))) (apply ~fn-name ~in))))
-
-
-(defn- do-remote-execute [{:keys [fn-name host port in timeout] :or {host "localhost" port 7888 timeout 1000}}]
-  (with-open [conn (nrepl/connect :host host :port port)]
-    (-> (nrepl/client conn timeout)
-        (nrepl/message {:op :eval :code (pr-str (call-with-require fn-name in))})
-        nrepl/combine-responses)))
-
-
-(defn remote-execute [params]
-  (let [{:keys [value err status]} (do-remote-execute params)]
-    (if (status "eval-error")
-      {:error err}
-      {:result (-> value last (or "") edn/read-string)})))
-
-
-(defn- local-execute [{:keys [fn-name in]}]
-  (try (let [result (eval (call-with-require fn-name in))]
-         {:result result})
-       (catch Exception e
-         {:error (.getMessage e)})))
-
-
-(def execute remote-execute)
-
+            [smeagol.configuration :refer [config]]
+            [smeagol.testing.execution :refer [execute]]))
 
 (defn do-test [{:keys [out] :as params}]
   (let [{:keys [error result]} (execute params)]
@@ -55,13 +26,21 @@
         fn-name (-> sym string/trim symbol)]
     (let [{:keys [value error line] :as x} (parse-value (or rest ""))]
       (if error
-        {:error (str "Failed parsing line: " line " due: " error)}
+        {:error (str "Failed parsing line due: " error)}
         (let [{:keys [in out] :or {in [] out nil}} value]
           {:fn-name fn-name :out out :in in :text text})))))
 
 
+(defn whitelist-namespace [{:keys [fn-name] :as params}]
+  (let [ns-name (namespace fn-name)]
+    (if-let [ns-config (-> config :test-namespaces (get ns-name))]
+      (merge ns-config params)
+      {:error (str "Namespace: " ns-name " is not listed: "
+                   (-> config :test-namespaces keys))})))
+
+
 (defn process [^String text ^Integer index]
-  (let [{:keys [error] :as params} (parse text)]
+  (let [{:keys [error] :as params} (-> text parse whitelist-namespace)]
     (if error
       (str "<pre class=\"test-result error\">" (pr-str params) "</pre><pre>" text "</pre>")
       (let [{:keys [result] :as test-result} (do-test params)]
