7d6ff40e19e188a624b5a36f8df729176e19766e
Vlad Bokov <razum2um@mail.ru>
2019-02-24 02:18:31 +0700

    Remote code execution via nrepl

--- src/smeagol/testing.clj
+++ src/smeagol/testing.clj
@@ -1,20 +1,46 @@
 (ns smeagol.testing
-  (:require [clojure.string :as s]
-            [clojure.edn :as edn]))
+  (:require [clojure.string :as string]
+            [clojure.edn :as edn]
+            [clojure.edn :as edn]
+            [nrepl.core :as nrepl]))
 
-(defn- execute [{:keys [fn in]}]
-  (try (let [result (apply fn in)]
+(defn- call-with-require [fn-name in]
+  (let [ns-sym (some-> fn-name namespace symbol)]
+    `(do ~(when ns-sym `(require (quote ~ns-sym))) (apply ~fn-name ~in))))
+
+
+(defn- do-remote-execute [{:keys [fn-name host port in timeout] :or {host "localhost" port 7888 timeout 1000}}]
+  (with-open [conn (nrepl/connect :host host :port port)]
+    (-> (nrepl/client conn timeout)
+        (nrepl/message {:op :eval :code (pr-str (call-with-require fn-name in))})
+        nrepl/combine-responses)))
+
+
+(defn remote-execute [params]
+  (let [{:keys [value err status]} (do-remote-execute params)]
+    (if (status "eval-error")
+      {:error err}
+      {:result (-> value last (or "") edn/read-string)})))
+
+
+(defn- local-execute [{:keys [fn-name in]}]
+  (try (let [result (eval (call-with-require fn-name in))]
          {:result result})
        (catch Exception e
          {:error (.getMessage e)})))
 
-(defn do-test [{:keys [fn in out] :as params}]
+
+(def execute remote-execute)
+
+
+(defn do-test [{:keys [out] :as params}]
   (let [{:keys [error result]} (execute params)]
     (if error
       {:result :error :error error}
       (if (= result out)
         {:result :ok}
         {:result :failure :expected out :actual result}))))
+
 
 (defn- parse-value [^String line]
   (try
@@ -24,30 +50,14 @@
       {:line line :error (.getMessage e)})))
 
 
-(defn- parse-values [texts]
-  (reduce (fn [acc text]
-            (let [{:keys [value error] :as parsed} (parse-value text)]
-              (if error
-                (reduced parsed)
-                (update-in acc [:values] conj value))))
-          {:values []}
-          texts))
-
-
 (defn parse [^String text]
-  (let [lines (s/split-lines text)]
-    (if (-> lines count (>= 3))
-      (let [[sym & rest] lines]
-        (if-let [fn (-> sym s/trim symbol resolve)] ;; TODO: require ns
-          (let [{:keys [values error line] :as x} (parse-values rest)]
-            (if error
-              {:error (str "Failed parsing line: " line " due: " error)}
-              (let [out (last values)
-                    in (butlast values)]
-                {:fn fn :out out :in in :text text})))
-          {:error (str "No test found with name: " (pr-str sym)) :text text}))
-      {:error (str "There shoud be at least 3 lines (test name, input, output), given: " (count lines))
-       :text text})))
+  (let [[sym rest] (string/split text #"\r?\n" 2)
+        fn-name (-> sym string/trim symbol)]
+    (let [{:keys [value error line] :as x} (parse-value (or rest ""))]
+      (if error
+        {:error (str "Failed parsing line: " line " due: " error)}
+        (let [{:keys [in out] :or {in [] out nil}} value]
+          {:fn-name fn-name :out out :in in :text text})))))
 
 
 (defn process [^String text ^Integer index]
